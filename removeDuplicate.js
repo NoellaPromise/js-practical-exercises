b7c392006b7671e26188d634521ba39eb46e3731
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-07-19 16:50:39 +0200

    Add Third question

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,7 +1,8 @@
 function removeDuplicate(array){
     let arr=[];
     for(let i=0; i<array.length;i++)
-    if(arr.indexOf(array[i] === -1))
+    if(arr.indexOf(array[i]) === -1)
     arr.push(array[i]);
     return arr;
     }
+    console.log(removeDuplicate(array=[1,2,1,3,2,4,4,5,2]));
